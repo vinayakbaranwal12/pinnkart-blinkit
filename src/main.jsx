f7b21b006150c323f822cb328d96c9ea9d633e9f
Nimish <bhardwajnimish93@gmail.com>
2025-07-23 17:24:53 +0530

    Fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 import "./index.css"; // Ensure your CSS file is imported
 
+// import { createRoot } from 'react-dom/client';
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
